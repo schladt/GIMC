facc226a1580096609ec673792e2533026176744
schladt <michael.schladt@gmail.com>
2023-08-29 20:36:36 -0400

    updated sysmom installer; collect.py

--- sandbox/sysmon/install.ps1
+++ sandbox/sysmon/install.ps1
@@ -1,20 +1,17 @@
-# Script to install Sysmon with config.xml
-# -u switch to uninstall Sysmon 
-
 param(
     [switch]$u
 )
 
 function Install-Sysmon {
     Write-Host "Installing Sysmon with config.xml..."
-    Start-Process -FilePath .\Sysmon64.exe -ArgumentList "-accepteula", "-i", ".\config.xml" -Wait
-    Write-Host "Sysmon Installed!"
+    & .\sysmon64.exe -accepteula -i .\config.xml
+
 }
 
 function Uninstall-Sysmon {
     Write-Host "Uninstalling Sysmon..."
-    Start-Process -FilePath .\Sysmon64.exe -ArgumentList "-u", "--force" -Wait
-    Write-Host "Sysmon Uninstalled!"
+    & .\sysmon64.exe -u
+
 }
 
 if ($u) {
