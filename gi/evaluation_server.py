0b996b1f783761a8e851d06112db01919b2e928b
Mike Schladt <michael.schladt@gmail.com>
2024-04-07 18:23:48 -0400

    editing comments

--- gi/evaluation_server.py
+++ gi/evaluation_server.py
@@ -11,13 +11,14 @@ import subprocess
 import json
 import shutil
 import requests
+import re
 
 from flask import Flask, jsonify, request
 from config import UNIT_TEST_FILE, SANDBOX_TOKEN, SANDBOX_URL
 
 # set up logging
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.ERROR,
     format='%(asctime)s - %(levelname)s - %(message)s',
 )
 
@@ -52,17 +53,28 @@ def submit():
 
     # compile the code
     outfilepath = os.path.join(tmp_dir, 'proto.dll')            
-    p = subprocess.Popen(['gcc.exe', '-shared', '-o', outfilepath, filepath], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    p = subprocess.Popen(['gcc.exe', '-Wall', '-shared', '-o', outfilepath, filepath], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     (out, err) = p.communicate()
     if p.returncode != 0:
-        logging.error(f"Error compiling code in ")
-        out = out.decode("utf-8")
+        logging.debug(f"Error compiling code in {filepath}")
+        out = out.decode("utf-8")   
         err = err.decode("utf-8")
-        logging.error(out)
-        logging.error(err)
-        # remove the tmp dir using shutil
+        logging.debug(out)
+        logging.debug(err)
+
+        # Use regular expressions to find all errors and warnings
+        errors = re.findall("error:", err)
+        warnings = re.findall("warning:", err)
+        compile_errors = {
+            "errors": len(errors),
+            "warnings": len(warnings)
+        }
+
         shutil.rmtree(tmp_dir)
-        return jsonify({"message": "error compiling code"}), 500
+        return jsonify({
+            "message": "error compiling code, num_errors",
+            "compile_errors": compile_errors           
+        }), 200
 
     # run the unit tests
     logging.info("Running unit tests in {}".format(UNIT_TEST_FILE))
@@ -73,8 +85,7 @@ def submit():
     if p.returncode != 0:
         logging.error(f"Error in unit test {UNIT_TEST_FILE} - {outfilepath}")
         logging.error(out)
-        # update database with error code (1)
-        # remove the tmp dir using shutil
+        
         shutil.rmtree(tmp_dir)
         return jsonify({"message": "error running unit tests"}), 500
 
@@ -86,85 +97,83 @@ def submit():
         num_tests = out["num_tests"]
     except:
         logging.error(f"Error decoding json in unit test {UNIT_TEST_FILE} - {outfilepath}")
-        # remove the tmp dir using shutil
+
         shutil.rmtree(tmp_dir)
         return jsonify({"message": "error decoding json from unit test output"}), 500
 
-    # if therea are no failures or errors, recompile and submit to sandbox
-    if num_failures == 0 and num_errors == 0:
-        # recompile the code
-        exefilepath = os.path.join(tmp_dir, 'proto.exe')
-        p = subprocess.Popen(['gcc.exe', '-o', exefilepath, filepath], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        (out, err) = p.communicate()
-        if p.returncode != 0:
-            logging.error(f"Error recompiling code in ")
-            out = out.decode("utf-8")
-            err = err.decode("utf-8")
-            logging.error(out)
-            logging.error(err)
-            # remove the tmp dir using shutil
-            shutil.rmtree(tmp_dir)
-            return jsonify({
-                "message": "error recompiling code",
-                "unit_test_results": {
-                    "num_failures": num_failures,
-                    "num_errors": num_errors,
-                    "num_tests": num_tests
-                }   }), 200
+    # recompile and submit to sandbox
+    exefilepath = os.path.join(tmp_dir, 'proto.exe')
+    p = subprocess.Popen(['gcc.exe', '-o', exefilepath, filepath], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    (out, err) = p.communicate()
+    if p.returncode != 0:
+        logging.error(f"error recompiling code as exe in {filepath}")
+        out = out.decode("utf-8")
+        err = err.decode("utf-8")
+        logging.error(out)
+        logging.error(err)
 
-        # submit to sandbox
-        logging.info("Submitting to sandbox")
+        shutil.rmtree(tmp_dir)
+        return jsonify({
+            "message": "error recompiling code as exe",
+            "unit_test_results": {
+                "num_failures": num_failures,
+                "num_errors": num_errors,
+                "num_tests": num_tests
+            }   }), 200
 
-        files = {
-            'file': exefilepath
-        }
-        headers = {
-            'Authorization': f'Bearer {SANDBOX_TOKEN}'
-        }
-        data = {
-            'analyze': 'true',
-            'tags': f'disposition=genome'
-        }
+    # submit to sandbox
+    logging.info("Submitting to sandbox")
+
+    files = {
+        'file': exefilepath
+    }
+    headers = {
+        'Authorization': f'Bearer {SANDBOX_TOKEN}'
+    }
+    data = {
+        'analyze': 'true',
+        'tags': f'disposition=genome'
+    }
+    try:
         r = requests.post(SANDBOX_URL + "/submit/sample", files=files, headers=headers, data=data)
-        if r.status_code != 200:
-            logging.error(f"Error submitting to sandbox")
-            shutil.rmtree(tmp_dir)
-            return jsonify({
-                "message": "error submitting to sandbox",
-                "unit_test_results": {
-                    "num_failures": num_failures,
-                    "num_errors": num_errors,
-                    "num_tests": num_tests
-                }           
-           }), 200
-        else:
-            logging.info(r.text)
-            logging.info("Submitted to sandbox")
-            # remove the tmp dir using shutil
-            shutil.rmtree(tmp_dir)
-            return jsonify({
-                "message": "successfully submitted to sandbox",
-                "unit_test_results": {
-                    "num_failures": num_failures,
-                    "num_errors": num_errors,
-                    "num_tests": num_tests
-                },
-                "sandbox_hashes": r.json()['hashes']         
-           }), 200
-
-
-
-
-    # remove the tmp dir using shutil
-    shutil.rmtree(tmp_dir)
-    return jsonify({
-        "num_failures": num_failures,
-        "num_errors": num_errors,
-        "num_tests": num_tests
-    })
-
-
-
+    except requests.exceptions.RequestException as e:
+        logging.error(f"error submitting to sandbox")
+        
+        shutil.rmtree(tmp_dir)
+        return jsonify({
+            "message": "error submitting to sandbox",
+            "unit_test_results": {
+                "num_failures": num_failures,
+                "num_errors": num_errors,
+                "num_tests": num_tests
+            }           
+        }), 200
+    if r.status_code != 200:
+        logging.error(f"error submitting to sandbox")
+        
+        shutil.rmtree(tmp_dir)
+        return jsonify({
+            "message": "error submitting to sandbox",
+            "unit_test_results": {
+                "num_failures": num_failures,
+                "num_errors": num_errors,
+                "num_tests": num_tests
+            }           
+        }), 200
+    else:
+        logging.info(r.text)
+        logging.info("submitted to sandbox")
+        
+        shutil.rmtree(tmp_dir)
+        return jsonify({
+            "message": "successfully submitted to sandbox",
+            "unit_test_results": {
+                "num_failures": num_failures,
+                "num_errors": num_errors,
+                "num_tests": num_tests
+            },
+            "sandbox_hashes": r.json()['hashes']         
+        }), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, threaded=True)
