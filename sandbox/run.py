2f5015d9b9116a0c0968bed3f2c96eac50f3bacf
Mike Schladt <michael.schladt@gmail.com>
2024-10-01 21:23:53 -0400

    update interface options

--- sandbox/run.py
+++ sandbox/run.py
@@ -1,7 +1,17 @@
 from app import create_app, db
 from app.models import *
+import sys
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
+    # get the first argument
+    if len(sys.argv) > 2:
+        interface = sys.argv[1]
+        port = sys.argv[2]
+    else:
+        # exit
+        print("Usage: python run.py <interface address> <port>")
+        sys.exit(1)
+
+    app.run(debug=True, host=interface, port=port, threaded=True)
