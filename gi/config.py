c6fa7bec6b8a43c72d7306f9558ab0233f3b7a9e
Mike Schladt <michael.schladt@gmail.com>
2026-01-09 17:40:50 -0500

    Updated classifier config settings

--- gi/config.py
+++ gi/config.py
@@ -8,3 +8,7 @@ with open(settings_file, 'r') as f:
 SANDBOX_TOKEN = settings['sandbox_token']
 SANDBOX_URL = settings['sandbox_url']
 UNIT_TEST_FILE = os.path.join('unit_tests', 'file_search_test.py')
+DATA_PATH = settings['data_path']
+CLASSIFIER_MODEL_PATH = os.path.join(DATA_PATH, 'classifier', 'model_data')
+CLASSIFIER_PATH = os.path.join(CLASSIFIER_MODEL_PATH, 'cnn4bsi_checkpoint.pth')
+TOKENIZER_PATH = os.path.join(CLASSIFIER_MODEL_PATH, 'mal_reformer')
